0ed56a8a81975be61a4296ec96ae7a0b6c1fc63f
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-03-25 19:34:26 -0400

    updated files

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,37 @@
 <template>
     <div class="container">
-        <div class="table">
-            <span class="left">
-                <span class="top">
-                    <img alt="Bone" src="../assets/bone.jpg" style="max-height: 200px;">
-                </span>
-                <span class="bottom">
-                    <img alt="Bone" src="../assets/bone.jpg" style="max-height: 200px;">
-                </span>
-            </span>
-            <span class="right">
-              <img alt="Photo" src="../assets/photo.jpg">
-            </span>
+        <div class="parent-table">
+            <div class="frame">
+                <div class="left">
+                    <div style="display: inline-block; float: right;">
+                        <img src="../assets/photo.jpg" alt="Photo">
+                    </div>
+                </div>
+                <div class="right" style="color: black;">
+                    <div class="timetable" style="padding-top: 10vh;margin-left: 50px">
+                        <div class="time">
+                            <span style="text-align: left;">姓名：林建中</span>
+                            <br/>
+                            <span style="text-align: left;">現任</span>
+                            <br/>
+                            <ul>
+                                <li v-bind:key="i" v-for="i in current">{{i}}</li>
+                            </ul>
+                            <span>學歷</span>
+                            <br>
+                            <ul>
+                                <li v-bind:key="i" v-for="i in education">{{i}}</li>
+                            </ul>
+                            <br>
+                            <div class="timebutton">
+                                <a>
+                                    <button class="tbutton"><span style="margin: 10px;">完整資訊</span></button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -21,6 +41,18 @@
         name: 'About',
         props: {
             msg: String
+        },
+        data() {
+            return {
+                current: ["國軍台中總醫院骨科部主任", "國防醫學院醫學系兼任助理教授"],
+                education: ["​國立中興大學材料科學與工程系博", "國防醫學院醫學士"],
+                experience_abridged: ["國軍台中總醫院中清分院副院長", "中華民國外科專科醫師", "中華民國骨科專科醫師",],
+                experience: ["國軍台中總醫院中清分院副院長", "國軍臺中總醫院教研部主任",
+                    "中華民國外科專科醫師", "中華民國骨科專科醫師", "台北榮總骨科部研究醫師", "中華民國脊椎專科醫師",
+                    "中華民國脊椎微創專科醫師", "中華民國關節重建專科醫師", "海岸巡防第八指揮部8848醫務所中尉醫官"],
+                member: ["中華民國脊椎外科醫學會會員", "中華民國關節重建醫學會會員",
+                    "臺灣脊椎微創醫學會會員", "臺灣英索關節醫學會會員", "台灣英索(Insall)人工關節學會會員"],
+            }
         }
     }
 </script>
@@ -31,7 +63,7 @@
         margin: 40px 0 0;
     }
 
-    ul {
+    /*ul {
         list-style-type: none;
         padding: 0;
     }
@@ -39,41 +71,102 @@
     li {
         display: inline-block;
         margin: 0 10px;
-    }
+    }*/
 
     a {
         color: #42b983;
     }
 
-    .table {
-        width: 100%;
-        line-height: 100px;
+    .notbold {
+        font-weight: normal
+    }
+
+    .parent-table {
+        text-align: center;
         position: relative;
     }
 
-    .left {
-        width: 50%;
-        background-color: #CCC;
+    .timebutton {
+        text-align: center;
+    }
+
+    .time {
+        text-align: left;
+    }
+
+    .timetable {
+        padding-top: 10px;
+        padding-left: 30px;
+        padding-right: 30px;
+        margin-top: 0;
+        display: flex;
+        width: 300px;
+        height: 360px;
+        color: black;
+        position: relative;
+        border-color: black;
+        border-width: 1px;
+    }
+
+    .frame {
         display: inline-block;
+        margin: 0 auto;
+    }
+
+    .left {
+        -ms-flex-order: 1;
+        order: 1;
+        -webkit-box-flex: 2; /* OLD - iOS 6-, Safari 3.1-6 */
+        -moz-box-flex: 2; /* OLD - Firefox 19- */
+        -webkit-flex: 2; /* Chrome */
+        -ms-flex: 2; /* IE 10 */
+        flex: 2; /* NEW, Spec - Opera 12.1, Firefox 20+ */
+        width: auto;
+        text-align: right;
+        /*background-color: #CCC;*/
+        display: inline-block;
+        float: left;
     }
 
     .right {
-        width: 50%;
-        text-align: right;
-        /*vertical-align: text-bottom;*/
+        -ms-flex-order: 2;
+        order: 2;
+        -webkit-box-flex: 1; /* OLD - iOS 6-, Safari 3.1-6 */
+        -moz-box-flex: 1; /* OLD - Firefox 19- */
+        -webkit-flex: 1; /* Chrome */
+        -ms-flex: 1; /* IE 10 */
+        flex: 1; /* NEW, Spec - Opera 12.1, Firefox 20+ */
+        color: white;
+        /*border: 1px solid black;*/
+        width: auto;
+        -webkit-box-ordinal-group: 2; /* OLD - iOS 6-, Safari 3.1-6 */
+        -moz-box-ordinal-group: 2; /* OLD - Firefox 19- */
+        /*width: 50%;*/
+        /*text-align: right;*/
         display: inline-block;
+
     }
 
-    .top {
-        height: 60%;
-        /*position: relative;*/
-
+    .tbutton {
         display: inline-block;
+        margin-left: 15px;
+        font-size: 20px;
+        border-color: #004091;
+        border-width: 2px;
+        color: #004091;
+        padding: 5px;
     }
 
-    .bottom {
-        height: 40%;
-        /*position: relative;*/
-        display: inline-block;
+    @media screen and (max-width: 700px) {
+        .frame {
+            -webkit-box-orient: vertical;
+            -moz-box-orient: vertical;
+            -webkit-box-direction: normal;
+            -moz-box-direction: normal;
+            /*box-orient: vertical;*/
+            -ms-flex-direction: column;
+            -webkit-flex-direction: column;
+            flex-direction: column;
+        }
     }
 </style>
