286b1ea3f7cced1dd4c8803e6a66ed8e50ba1fba
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-03-26 11:20:43 -0400

    updated files

--- src/views/About.vue
+++ src/views/About.vue
@@ -46,12 +46,20 @@
             return {
                 current: ["國軍台中總醫院骨科部主任", "國防醫學院醫學系兼任助理教授"],
                 education: ["​國立中興大學材料科學與工程系博", "國防醫學院醫學士"],
-                experience_abridged: ["國軍台中總醫院中清分院副院長", "中華民國外科專科醫師", "中華民國骨科專科醫師",],
                 experience: ["國軍台中總醫院中清分院副院長", "國軍臺中總醫院教研部主任",
                     "中華民國外科專科醫師", "中華民國骨科專科醫師", "台北榮總骨科部研究醫師", "中華民國脊椎專科醫師",
                     "中華民國脊椎微創專科醫師", "中華民國關節重建專科醫師", "海岸巡防第八指揮部8848醫務所中尉醫官"],
                 member: ["中華民國脊椎外科醫學會會員", "中華民國關節重建醫學會會員",
                     "臺灣脊椎微創醫學會會員", "臺灣英索關節醫學會會員", "台灣英索(Insall)人工關節學會會員"],
+                research: ["Ming-Chin Lu, Tung-Yuan Lai, Jin-Ming Hwang, Hsien-Te Chen, Sheng-Huang Chang, Fuu-Jen Tsai, Hwai-Lee Wang, Chien-Chung Lin, Wei-Wen Kuo, Chih-Yang Huang. Proliferation- and migration-enhancing effects of ginseng and ginsenoside Rg1 through IGF-I- and FGF-2-signaling pathways on RSC96 Schwann cells. Cell Biochem Funct. 2009; 27: 186-192._",
+                    "Kun Shan Huang, Min-Jin Lu, Yueh-Sheng Chen, Fuu-Jen Tsai, Wei-Wen Kuo, Yi-Chang Cheng, Chien-Chung Lin, Chang-Hai Tsai, Chih-Yang Huang, Jaung-Geng Lin, Hsiu-Chung Ou. Proliferative Effects of Chishao on Schwann Cells are FGF-uPA, and ERK- and JNK-Dependent. The American Journal of Chinese Medicine. 2009; 37(6): 1191-1202.",
+                    "Wen-Kuei Fang, Yi-Jiun Weng, Mu-Hsin Chang, Chien-Chung Lin, Yueh-Sheng Chen, Hsi-Hsien Hsu, Fuu-Jen Tsai, Chang-Hai Tsai, Wu-Hsien Kuo,_Chung Yen Lu, Chih-Yang Huang. Proliferative Effects of Chishao on Injured Peripheral Neurons. The American Journal of Chinese Medicine. 2010; 38(4): 735-743. ",
+                    "Yung-Ming Chang, Wei-Yi Chi, Tung-Yuan Lai, Yueh-Sheng Chen, Fuu-Jen Tsai, Chang-Hai Tsai, Wei-Wen Kuo, Yi-Chang Cheng, Chien-Chung Lin, Chih-Yang Huang. Dilong: Role in Peripheral Nerve Regeneration. Evidence-Based Complementary and Alternative Medicine. 2011; doi:10.1093/ecam/neq079.",
+                    "Chi-Chuan Yang, Chien-Chung Lin, Shiow-Kang Yen.. _Electrochemical Deposition of Vancomycin/Chitosan Composite _on Ti Alloy. _Journal of The Electrochemical Society. 2011; 158: E1-E12.", "Chi-Chuan Yang, Chiang-Yu Huang, Chien-Chung Lin, Shiow-Kang Yen.  Electrolytic Deposition of Collagen/HA Composite on Post HAOTiO2 Coated Ti6Al4V Implant Alloy. Journal of The Electrochemical Society. 2011; 58 (2): E13-E20.",
+                    "Jui-Lung Shen, Yueh-Sheng Chen, Jing-Ying Lin, Yun-Chen Tien, Wen-Huang Peng, Chia-Hua Kuo, Bor-Show Tzang, Hwai-Lee Wang, Fuu-Jen Tsai, Ming-Chih Chou, Chih-Yang Huang, Chien-Chung Lin. Neuron Regeneration and Proliferation Effects of Danshen and Tanshinone IIA. Evidence-Based Complementary and Alternative Medicine. 2011; doi:10.1155/2011/378907.",
+                    "Chi-Chuan Yang, Chien-Chung Lin, Jiunn-Wang Liao , Shiow-Kang Yen. Vancomycin-chitosan composite deposited on post porous hydroxyapatite coated Ti6Al4V implant for drug controlled release. Materials Science and Engineering C. 2013; 33: 2203-2212.",
+                    "Chien-Chung Lin, Pei-Yu Chao, Chia-Yao Shen, Jyuan-Jen Shu, Shiow-Kang Yen, Chih-Yang Huang, Jer-Yuh Liu. Novel Target Genes Responsive to Apoptotic Activity by Ocimum gratissimum Extracts in Human Osteosarcoma Cells. The American Journal of Chinese Medicine. 2014; 42(3): 1-25.",
+                    "Wei-Min Chi, Chien-Chung Lin, YingJui Ho, Hsiao-Che Lin & Jian-Horng Chen. Using nonlinear finite element models to analyse stress distribution during subluxation and torque required for dislocation of newly developed total hip structure after prosthetic impingement. Medical & Biological Engineering & Computing. 2018; 56(1): 37-47."],
             }
         }
     }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png">
         <Carousel/>
-
+        <img alt="Vue logo" src="../assets/logo.png">
         <Home msg="Hello"></Home>
     </div>
 </template>
