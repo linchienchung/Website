3963122ffb9764a48946a18940ea335ca3ee8ed4
Thomas <37830237+Thomasqazwsxedc@users.noreply.github.com>
2020-03-26 08:38:44 -0400

    updated files

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/Website/'
+        : '/',
     chainWebpack: config => {
         config.module
             .rule('vue')
